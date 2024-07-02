a4c38dfddb4656566773a75625b573c62749a912
DwiAbdulK <156380543+DwiAbdulK@users.noreply.github.com>
2024-07-02 14:11:14 +0700

    Update program binary.py

--- program binary.py
+++ program binary.py
@@ -53,12 +53,18 @@ def main():
             break
         
         else:
-            print("Pilihan tidak valid. Masukkan pilihan 1 sampai 5")
+            print("Pilihan yang anda masukan tidak valid. Masukkan pilihan 1 sampai 5")
 
-def linear_search(arr, x):
-    for i in range(len(arr)):
-        if arr[i] == x:
-            return i
+def binary_search(arr, x):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] < x:
+            left = mid + 1
+        else:
+            right = mid - 1
     return -1
 
 if __name__ == "__main__":
